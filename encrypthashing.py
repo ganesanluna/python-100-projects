b01bb0884d92203009ed8a0610801b0973652618
ganeshluna <lunaganesh@outlook.com>
2017-06-09 14:14:41 +0530

    Add files via upload

---
+++ encrypthashing.py
@@ -0,0 +1,14 @@
+_author="BaluGanesan"
+import hashlib
+i = int(raw_input("hashing value : "))
+e=raw_input("enter your password : ")
+while i >= 0:
+	a=hashlib.md5(str(e)).hexdigest()
+	b=hashlib.sha1(str(a)).hexdigest()
+	c=hashlib.sha224(str(b)).hexdigest()
+	d=hashlib.sha384(str(c)).hexdigest()
+	e=hashlib.sha512(str(d)).hexdigest()	
+	i = i - 1
+print e
+
+	
